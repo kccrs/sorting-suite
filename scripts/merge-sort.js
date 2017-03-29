cc61b1126456056069f4a7fef0e444f1a0cbeaff
Casey Cross <hellocaseycross@gmail.com>
2017-03-28 23:50:45 -0600

    Refactored merge sort.

--- scripts/merge-sort.js
+++ scripts/merge-sort.js
@@ -1,35 +1,36 @@
 const MergeSort = (arr) => {
   // let testArray = [8, 47, 11, 6, 16, 30];
   // let testArray = [3, 99, 11, 6, 19, 2, 34];
-
   if (arr.length <= 1) {
     return arr;
   }
 
   let middle = Math.floor(arr.length / 2);
+  // let leftSide = arr.slice(0, middle);
+  // let mergeLeft = MergeSort(leftSide);
+  // ^^ above is refactored into below
+  // let mergeLeft = MergeSort(arr.slice(0, middle));
+  // let rightSide = arr.slice(middle);
+  // let mergeRight = MergeSort(rightSide);
+  // ^^ above is refactored into below
+  // let mergeRight = MergeSort(arr.slice(middle));
 
-  let leftSide = arr.slice(0, middle);
-  let mergeLeft = MergeSort(leftSide);
-
-  
-  let rightSide = arr.slice(middle);
-  let mergeRight = MergeSort(rightSide);
-  return mergeIt(mergeLeft, mergeRight);
+  // return mergeIt(mergeLeft, mergeRight);
+  return mergeIt(MergeSort(arr.slice(0, middle)), MergeSort(arr.slice(middle)));
 };
 
 const mergeIt = (left, right) => {
   let sortedArray = [];
 
   while (left.length && right.length) {
-    if (left[0] <= right[0]) {
-      sortedArray.push(left.shift());
-    } else {
-      sortedArray.push(right.shift());
-    }
+    sortedArray.push((left[0] <= right[0]) ? left.shift() : right.shift());
+    // if (left[0] <= right[0]) {
+    //   sortedArray.push(left.shift());
+    // } else {
+    //   sortedArray.push(right.shift());
+    // }
   }
-
   return sortedArray.concat(left.length ? left : right);
-
 };
 
 module.exports = MergeSort;
