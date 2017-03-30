332f0ed4a1ffc25ab2b7179f077fa04e164f42a8
Casey Cross <hellocaseycross@gmail.com>
2017-03-30 15:27:47 -0600

    Finish merge sort and clean up code.

--- scripts/merge-sort.js
+++ scripts/merge-sort.js
@@ -1,6 +1,4 @@
 const MergeSort = (arr) => {
-  // let testArray = [8, 47, 11, 6, 16, 30];
-  // let testArray = [3, 99, 11, 6, 19, 2, 34];
   if (arr.length <= 1) {
     return arr;
   }
@@ -24,11 +22,6 @@ const mergeIt = (left, right) => {
 
   while (left.length && right.length) {
     sortedArray.push((left[0] <= right[0]) ? left.shift() : right.shift());
-    // if (left[0] <= right[0]) {
-    //   sortedArray.push(left.shift());
-    // } else {
-    //   sortedArray.push(right.shift());
-    // }
   }
   return sortedArray.concat(left.length ? left : right);
 };
